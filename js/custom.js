c033d53fabbfa0f33f793037e44d15d4a90dec23
MaxVell89 <max_vell_89@mail.ru>
2017-02-05 22:37:43 +0300

    Scrolling-js-menu and modal-window

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,8 @@
 $(function() {
 
-	/* Smooth scroll to plans */
+	/* Плавный скрол до блока с тарифами 
+    ========================================*/
+
     $('#js-get-started').on('click', function(e) {
     	e.preventDefault();
     	var plansOffset = $('#js-plans').offset().top;    // расстояние от верха(top) до элемента
@@ -11,21 +13,56 @@ $(function() {
     });
 
 
-    /* Fixed header when scroll */
+    /* Фиксированное верхнее меню при скроле за первый экран 
+    ============================================================*/
+
     var headerH = $('#js-header').height(),        // узнаем высоту элемента
-    		navH = $('#js-nav').innerHeight();     // полная высота элемента (с padding)
+    		navH = $('#js-nav-container').innerHeight();     // полная высота элемента (с padding)
 
     $(document).on('scroll', function() {
     	var documentScroll = $(this).scrollTop();   // расстояние скрола до верха
 
     	if (documentScroll > headerH) {
-    		$('#js-nav').addClass('fixed');
+    		$('#js-nav-container').addClass('fixed');
     		$('#js-header').css('paddingTop', navH);
     	} else {
-    		$('#js-nav').removeClass('fixed');
+    		$('#js-nav-container').removeClass('fixed');
     		$('#js-header').removeAttr('style');
     	}
 
     });
+
+
+    /* Скролинг при нажатии ссылок меню 
+    =======================================*/
+
+    $('#js-nav a').on('click', function(e) {
+        e.preventDefault();
+        var currentBlock = $(this).attr('href'),    // Узнаем значение атрибута href
+            currentBlockOffset = $(currentBlock).offset().top;   // Узнаем расстояние от верха страницы до блока
+        $('html, body').animate({
+            scrollTop: currentBlockOffset - 20    // Анимация скрола от верха документа до блока - 20 пикселей
+        }, 500);
+        }
+    );
+
+
+    /* Модальные окна
+    =======================================*/
+
+    $('.js-modal-show').on('click', function(e) {
+        e.preventDefault(); 
+
+        var currentModal = $(this).attr('href');
+        $(currentModal + ', #js-owerlay').fadeIn();  // Показаваем модальное окно
+        $('body').addClass('open-modal');            // Добавляем класс отменяющий скроллинг
+    });
+
+    $('.js-modal-close, #js-owerlay').on('click', function(e) {
+        e.preventDefault();
+        $('.js-modal, #js-owerlay').fadeOut();       // Скрываем модальное окно
+        $('body').removeClass('open-modal');        // Убираем класс отменяющий скроллинг
+    });
+
 
 });
